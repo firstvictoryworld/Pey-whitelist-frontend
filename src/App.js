ac83ad73ae216e86eb1608325df618a9bd094467
admin <admin@admin.com>
2021-03-09 12:43:07 +0100

    done

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Admin, Resource } from 'react-admin';
+import dataProvider from './dataProvider';
+import NotesList from './components/NotesList';
+import NotesEdit from './components/NotesEdit';
+import NotesCreate from './components/NotesCreate';
+
+function App() {
+  return (
+    <Admin dataProvider={dataProvider}>
+      <Resource
+        name="whitelists"
+        list={NotesList}
+        edit={NotesEdit}
+        create={NotesCreate}
+      />
+    </Admin>
+  );
+}
+
+export default App;
