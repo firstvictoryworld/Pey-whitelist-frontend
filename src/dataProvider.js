3155e06e85957bfc88d5c1fe00a50d18e7910967
admin <admin@admin.com>
2021-03-16 11:29:46 +0100

    update endpoint

--- src/dataProvider.js
+++ src/dataProvider.js
@@ -1,7 +1,7 @@
 import { fetchUtils } from 'react-admin';
 import { stringify } from 'query-string';
 
-const apiUrl = 'http://198.199.72.22/api';
+const apiUrl = 'https://admin.pey.cash/api';
 const httpClient = fetchUtils.fetchJson;
 
 export default {
